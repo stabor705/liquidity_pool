0e2cec56948cb1db43919efcb45fc01099006c2f
Stanisław Borowy <stabor@startmail.com>
2022-11-20 16:48:25 +0100

    Add price parameters and some general fixes

--- src/calc.rs
+++ src/calc.rs
@@ -1,13 +1,17 @@
 use crate::error::{LiqPoolError, Result};
 
 /// How 1 token is represented in u64 number.
-/// Values less than UNIT are fractions and 1 is the smallest unit (ex. lamport in SOL).
+/// Values less than UNIT are fractions. 1 is the smallest unit (ex. lamport in SOL).
 pub const UNIT: u64 = 1000000000;
 
 /// Calculate amount * (nominator / denominator)
 pub fn propotion(amount: u64, nominator: u64, denominator: u64) -> Result<u64> {
     u64::try_from((amount as u128 * nominator as u128) / denominator as u128)
         .map_err(|_| LiqPoolError::CalculationError)
+}
+
+pub fn value(amount: u64, price: u64) -> Result<u64> {
+    propotion(amount, price, UNIT)
 }
 
 /// Calculate someone's share after adding `value` to pool with `total_value`
@@ -24,5 +28,5 @@ pub fn shares(value: u64, total_value: u64, total_shares: u64) -> Result<u64> {
 /// Given amount and a fee represented as a fraction in u64, calculate
 /// amount with subtracted fee.
 pub fn apply_fee(amount: u64, fee: u64) -> Result<u64> {
-    Ok(amount - propotion(amount, fee, UNIT)?)
+    Ok(amount - value(amount, fee)?)
 }
